2486b9859c1224c598e1b9a61c670c41af1ac7be
2pk <pankajnarula@gmail.com>
2016-12-10 12:47:03 -0800

    added linkedin, twitter

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to content repo on GitHub...\033[0m"
+
+git add -A
+msg = "rebuilding site `date`"
+if [ $# -eq 1 ]
+          then msg="$1"
+fi
+
+git commit -m "$msg"
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
@@ -9,6 +17,8 @@ hugo # if using a theme, replace by `hugo -t <yourtheme>`
 cd public
 # Add changes to git.
 git add -A
+
+echo -e "\033[0;32mDeploying updates to static html repo on GitHub...\033[0m"
 
 # Commit changes.
 msg="rebuilding site `date`"
