6fe0b3fc5cd8f2d64e193147aa99c162edcd5950
2pk <pankajnarula@gmail.com>
2016-12-10 12:49:31 -0800

    rebuilding site Sat Dec 10 12:49:31 PST 2016

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@
 echo -e "\033[0;32mDeploying updates to content repo on GitHub...\033[0m"
 
 git add -A
-msg = "rebuilding site `date`"
+
+msg="rebuilding site `date`"
+
 if [ $# -eq 1 ]
           then msg="$1"
 fi
 
 git commit -m "$msg"
+git push
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
