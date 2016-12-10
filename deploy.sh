62ab7131a6f69185a23a1cc1e008465e055b5655
2pk <pankajnarula@gmail.com>
2016-12-10 12:51:28 -0800

    rebuilding site Sat Dec 10 12:51:28 PST 2016

--- deploy.sh
+++ deploy.sh
@@ -24,14 +24,10 @@ git add -A
 echo -e "\033[0;32mDeploying updates to static html repo on GitHub...\033[0m"
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-      then msg="$1"
-      fi
-      git commit -m "$msg"
+git commit -m "$msg"
 
-      # Push source and build repos.
-      git push origin master
+# Push source and build repos.
+git push origin master
 
-      # Come Back
-      cd ..
+# Come Back
+cd ..
